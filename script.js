34ea27c05770cb405d7708b30a190c6360aa5d00
Rjoaozinho1 <82383559+Rjoaozinho1@users.noreply.github.com>
2023-05-11 12:06:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function buscarEndereco() {
+  const cep = document.getElementById('cep').value;
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const endereco = `${data.logradouro}, ${data.bairro}, ${data.localidade}, ${data.uf}`;
+      document.getElementById('endereco').textContent = endereco;
+    })
+    .catch(error => {
+      console.error('Erro ao buscar endereço:', error);
+    });
+}
